9095bdcd2388e13c52cd9cfcb04081519b201161
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-06-09 23:39:52 -0400

    feat: Added bottom navigation
    Bottom navigatio bar was added for the Main Acitivity. Also color palette was defined,
    changed color and added new icons for the bottom navbar.
    
    Strings for i18n added

---
+++ app/src/main/java/com/sbaltazar/pemucoffee/ui/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sbaltazar.pemucoffee.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sbaltazar.pemucoffee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
